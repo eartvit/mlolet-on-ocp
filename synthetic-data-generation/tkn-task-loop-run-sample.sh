6a4b59e56063b30d151c12f6db0953c206ac4236
Arthur Vitui <avitui@redhat.com>
2024-06-07 20:20:55 -0400

    Added node_exporter for Prometheus variant

--- synthetic-data-generation/tkn-task-loop-run-sample.sh
+++ synthetic-data-generation/tkn-task-loop-run-sample.sh
@@ -2,7 +2,7 @@
 
 tkn task start launch-loop-auto-lt-pipeline-task --timeout 0 --param RUNS=3 --param randReqMode=True --param randPayload=True \
     --param connectionsLowerBound=5 --param connectionsUpperBound=25 \
-    --param durationLowerBound=3600 --param durationUpperBound=10800 \
+    --param durationLowerBound=360 --param durationUpperBound=720 \
     --param createSpikes=True --param spikeConnections=15 --param randomSpikeDuration=False --param randomSpikeRepeat=False \
     --param spikeDurationLoBound=30 --param spikeDurationUpBound=31 \
     --param spikeRepetitionLoBound=180 --param spikeRepetitionUpBound=181
